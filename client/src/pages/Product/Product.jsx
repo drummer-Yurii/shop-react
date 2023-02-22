ee5ba12f9d463886c5c1ffd584615c735293f209
Yurii Rybachok <drummer2009@ukr.net>
2023-02-22 14:24:35 +0200

    add products to cart with redux

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -5,10 +5,12 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import BalanceIcon from '@mui/icons-material/Balance';
 import useFetch from '../../hooks/useFetch';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../redux/cartReducer';
 
 const Product = () => {
   const id = useParams().id;
-  const [selectedImg, setSelectedImg] = useState("img");
+  const [selectedImg, setSelectedImg] = useState('img');
   const [quantity, setQuantity] = useState(1);
 
   // const images = [
@@ -16,6 +18,7 @@ const Product = () => {
   //   'https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
   // ];
 
+  const dispatch = useDispatch();
   const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
 
   return (
@@ -27,32 +30,53 @@ const Product = () => {
           <div className="left">
             <div className="images">
               <img
-                src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img?.data?.attributes?.url}
+                src={
+                  process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img?.data?.attributes?.url
+                }
                 alt=""
-                onClick={(e) => setSelectedImg("img")}
+                onClick={(e) => setSelectedImg('img')}
               />
               <img
-                src={process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img2?.data?.attributes?.url}
+                src={
+                  process.env.REACT_APP_UPLOAD_URL + data?.attributes?.img2?.data?.attributes?.url
+                }
                 alt=""
-                onClick={(e) => setSelectedImg("img2")}
+                onClick={(e) => setSelectedImg('img2')}
               />
             </div>
             <div className="mainImg">
-              <img src={process.env.REACT_APP_UPLOAD_URL + data?.attributes[selectedImg]?.data?.attributes?.url} alt="" />
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes[selectedImg]?.data?.attributes?.url
+                }
+                alt=""
+              />
             </div>
           </div>
           <div className="right">
             <h1>{data?.attributes?.title}</h1>
             <span className="price">${data?.attributes?.price}</span>
-            <p>
-              {data?.attributes?.desc}
-            </p>
+            <p>{data?.attributes?.desc}</p>
             <div className="quantity">
               <button onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}>-</button>
               {quantity}
               <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
             </div>
-            <button className="add">
+            <button
+              className="add"
+              onClick={() =>
+                dispatch(
+                  addToCart({
+                    id: data.id,
+                    title: data.attributes.title,
+                    desc: data.attributes.desc,
+                    price: data.attributes.price,
+                    img: data.attributes.img.data.attributes.url,
+                    quantity,
+                  }),
+                )
+              }>
               <AddShoppingCartIcon /> ADD TO CART
             </button>
             <div className="links">
