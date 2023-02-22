3d3868c7b796450a4e7f594dd490fec43e7a4e6c
Yurii Rybachok <drummer2009@ukr.net>
2023-02-22 18:52:08 +0200

    refactor....

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -7,53 +7,33 @@ import { makeRequest } from '../../makeRequest';
 import { loadStripe } from '@stripe/stripe-js';
 
 const Cart = () => {
-  // const data = [
-  //   {
-  //     id: 1,
-  //     img: 'https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600',
-  //     img2: 'https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600',
-  //     title: 'Long Sleeve Graphic T-shirt',
-  //     desc: 'Long Sleeve Graphic T-shirt',
-  //     isNew: true,
-  //     oldPrice: 19,
-  //     price: 12,
-  //   },
-  //   {
-  //     id: 2,
-  //     img: 'https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600',
-  //     title: 'Coat',
-  //     desc: 'Coat',
-  //     isNew: true,
-  //     oldPrice: 19,
-  //     price: 12,
-  //   },
-  // ];
-
   const products = useSelector((state) => state.cart.products);
   const dispatch = useDispatch();
 
   const totalPrice = () => {
     let total = 0;
     products.forEach((item) => {
-      total += item.quantity * item.price
+      total += item.quantity * item.price;
     });
     return total.toFixed(2);
   };
 
-  const stripePromise = loadStripe('pk_test_51LutNKF5pBtNoeM0YTjgq3bZr67G8EuEsw1uWosVsQFWONk6XJ98EfQQEOYH3JYCDDtTvX8IKQryuM93Eiy2hc7R000neW08az');
+  const stripePromise = loadStripe(
+    'pk_test_51LutNKF5pBtNoeM0YTjgq3bZr67G8EuEsw1uWosVsQFWONk6XJ98EfQQEOYH3JYCDDtTvX8IKQryuM93Eiy2hc7R000neW08az',
+  );
 
   const handlePayment = async () => {
     try {
       const stripe = await stripePromise;
 
-      const res = await makeRequest.post("/orders", {
+      const res = await makeRequest.post('/orders', {
         products,
       });
 
       await stripe.redirectToCheckout({
         sessionId: res.data.stripeSession.id,
       });
-    } catch(err) {
+    } catch (err) {
       console.log(err);
     }
   };
@@ -67,7 +47,10 @@ const Cart = () => {
           <div className="details">
             <h1>{item.title}</h1>
             <p>{item.desc?.substring(0, 100)}</p>
-            <div className="price"> {item.quantity} x ${item.price} </div>
+            <div className="price">
+              {' '}
+              {item.quantity} x ${item.price}{' '}
+            </div>
           </div>
           <DeleteOutlinedIcon className="delete" onClick={() => dispatch(removeItem(item.id))} />
         </div>
@@ -77,7 +60,9 @@ const Cart = () => {
         <span>${totalPrice()}</span>
       </div>
       <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
-      <span className="reset" onClick={() => dispatch(resetCart())}>Reset Cart</span>
+      <span className="reset" onClick={() => dispatch(resetCart())}>
+        Reset Cart
+      </span>
     </div>
   );
 };
